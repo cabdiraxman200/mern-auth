52bc8f8cd055daeab8d00c79c6d02446d548f764
cabdiraxman200 <145711376+cabdiraxman200@users.noreply.github.com>
2024-06-02 10:25:49 +0300

    create signu api route

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 const app=express()
+app.use(express.json());
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("connected to MongoDB");
     
@@ -17,3 +19,4 @@ console.log("server listening on port 3000");
 }) 
 //api rutes
 app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
